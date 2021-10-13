7736482c820c84779c7ab52652ce04576932f331
AlexkyTD <alexisveillon@hotmail.fr>
2021-10-13 10:40:21 +0200

    add firebase features

--- Source/CatRush.Target.cs
+++ Source/CatRush.Target.cs
@@ -10,6 +10,13 @@ public class CatRushTarget : TargetRules
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 
-		ExtraModuleNames.AddRange( new string[] { "CatRush" } );
+        ExtraModuleNames.AddRange(new string[] { "CatRush" });
+
+        if (Target.Platform == UnrealTargetPlatform.Android)
+		{
+			ExtraModuleNames.Add("OnlineSubsystemGooglePlay");
+			ExtraModuleNames.Add("OnlineSubsystem");
+			ExtraModuleNames.Add("AndroidAdvertising");
+		}
 	}
 }
