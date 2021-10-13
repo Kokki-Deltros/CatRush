aa96af961cb339ae0727b60480d8fb89ffcad4c1
AlexkyTD <alexisveillon@hotmail.fr>
2021-10-13 17:02:30 +0200

    Add Firebase feature + try to fix ads

--- Source/CatRush/CatRush.Build.cs
+++ Source/CatRush/CatRush.Build.cs
@@ -14,9 +14,11 @@ public class CatRush : ModuleRules
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.Add("AndroidAdvertising");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
